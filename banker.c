fa92f02935506287208375db136f2131ca452241
telmo-wq <telmomcalheiros@gmail.com>
2026-05-18 21:23:50 -0300

    Matrizes para as estruturas de recursos

--- banker.c
+++ banker.c
@@ -6,9 +6,19 @@
 int main(int argc, char *argv[]){
     
 
-    FILE *arquivo_com = fopen("commands.txt", "r");
+    FILE *arquivo_com = fopen("commandos.txt", "r");
+
+    if (arquivo_com == NULL){
+        printf("ERRO! Arquivo inexistente\n");
+        return 0;
+    }
+
     FILE *arquivo_cli = fopen("customer.txt", "r");
 
+    if (arquivo_cli == NULL){
+        printf("ERRO! Arquivo inexistente\n");
+        return 0;
+    }
 
     
 }
--- commands.c
+++ commands.c
@@ -3,48 +3,81 @@
 #include <string.h>
 
 
-void commands(FILE *arquivo_com, FILE *arquivo_cli, int argc){
+int contar_clientes(FILE *arquivo_cli){
     char linha[200];
-    char op[10];
-    int customer;
-    int offset = 0;
-    int consumed = 0;
+    int clientes = 0;
 
-    
-    while (fgets(linha, sizeof(linha), arquivo_com) != NULL) {
-    char op[10];
-    int customer;
-    int offset = 0;
-    int consumed = 0;
-
-    // lendo o código da operação (rq ou rl)
-    sscanf(linha, "%s%n", op, &consumed);
-    offset += consumed;
-
-    // tratar o * separado
-    if (strcmp(op, "*") == 0) {
-        // operação exibir
-        continue;
+    while(fgets(linha, sizeof(linha), arquivo_cli) != NULL){
+        clientes++;
     }
 
-    // lendo o número do customer
-    sscanf(linha + offset, "%d%n", &customer, &consumed);
-    offset += consumed;
-
-    // lê o restante dos recursos
-    int number_of_resources[argc];
-    for (int i = 0; i < number_of_resources; i++) {
-        if (sscanf(linha + offset, "%d%n", &number_of_resources[i], &consumed) != 1) {
-            // erro de formato
-        }
-        offset += consumed;
-    }
-
-    if (strcmp(op, "RQ") == 0) {
-        // operação request
-    } else if (strcmp(op, "RL") == 0) {
-        // operação release
-    }
+    rewind(arquivo_cli);
+    return clientes;
 }
+
+void commands(FILE *arquivo_com, FILE *arquivo_cli, int argc){
+    int number_of_customers = contar_clientes(arquivo_cli);
+    char linha[200];
+    int recursos[argc];
+    int max[number_of_customers][argc];
+    int need[number_of_customers][argc];
+    int allocation[number_of_customers][argc];
+    int avaiable[number_of_customers][argc];
+
+    FILE *log = fopen("result.txt", "w");
+
+    while(fgets(linha, sizeof(linha), arquivo_cli) != NULL){  //percorre arquivo_cli para preencher o vetor max[]
+        int offset = 0;
+        int consumed = 0;
+
+        for (int i = 0; i < argc; i++){
+            if (sscanf(linha + offset, "%d, %n", &max[i], &consumed)){
+                //trata erro de formatação
+            }
+            offset += consumed;
+        }
+    }
+
+    for (int i = 0; i < number_of_customers; i++){
+        for (int j = 0; j < argc; j++){
+            avaiable[i][j] = 0;
+        }
+    }
+    
+    while (fgets(linha, sizeof(linha), arquivo_com) != NULL) {  //percorrendo cada linha do arquivo_com
+        char op[10];
+        int customer;
+        int offset = 0;
+        int consumed = 0;
+
+        // lendo o código da operação (rq ou rl)
+        sscanf(linha, "%s%n", op, &consumed);
+        offset += consumed;
+
+        // tratar o * separado
+        if (strcmp(op, "*") == 0) {
+            // operação exibir
+            continue;
+        }
+
+        // lendo o número do customer
+        sscanf(linha + offset, "%d%n", &customer, &consumed);
+        offset += consumed;
+
+        // lê o restante dos recursos
+        for (int i = 0; i < argc; i++) {
+            if (sscanf(linha + offset, "%d%n", &avaiable[i], &consumed) != 1) {
+                // erro de formato
+            }
+            offset += consumed;
+
+        }
+
+        if (strcmp(op, "RQ") == 0) {
+            
+        } else if (strcmp(op, "RL") == 0) {
+            // operação release
+        }
+    }
 }
 
