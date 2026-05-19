556efc333c83f5f429397acebe8a3aae2aea9753
telmo-wq <telmomcalheiros@gmail.com>
2026-05-19 20:38:00 -0300

    is_safe escrito

--- commands.c
+++ commands.c
@@ -14,6 +14,50 @@ int contar_clientes(FILE *customers){
     return clientes;
 }
 
+//simula se a alocação de recursos é segura
+//work e finish são vetores cópia de recursos[] e avaiable[]
+int is_safe(int work[], int finish[], int **need, int number_of_costumers, int argc, int **allocation){
+    int progresso = 1;
+
+    while(progresso == 1){
+        progresso = 0;
+
+        for (int i = 0; i < number_of_costumers; i++){  //para cada costumer
+            int flag = 0;
+            if (finish[i] == 1){
+                continue;
+            }
+
+            for (int j = 0; j < argc; j++){   //para cada recurso
+                if (need[i][j] > work[j]){
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0){
+                for (int j = 0; j < argc; j++){
+                    work[j] += allocation[i][j];
+                }
+                finish[i] = 1;
+                progresso = 1;
+
+            }   
+        }
+    }
+
+    int safe = 1;
+    for (int i = 0; i < number_of_costumers; i++){
+        if (finish[i] == 0){
+            safe = 0;
+            break;
+        }
+    }
+    if (safe){
+        return 1;
+    }else {
+        return 0;
+    }
+}
 
 //lembrar que avaiable[] é o array de instâncias disponíveis dos recursos passados na linha de comando
 void ler_comandos(FILE *commands, FILE *customers, int argc, int avaiable[]){   //argc é o número de recursos
@@ -93,7 +137,7 @@ void ler_comandos(FILE *commands, FILE *customers, int argc, int avaiable[]){   
                     break;
                 }
             }
-            
+
             for (int j = 0; j < argc; j++){
                 if (recursos[j] > avaiable[j]){
                     not_enough = 1;
@@ -122,6 +166,19 @@ void ler_comandos(FILE *commands, FILE *customers, int argc, int avaiable[]){   
                 fprintf(log, "\n");
                 continue;
             }
+
+            int work[argc-1];
+            int finish[number_of_customers];
+
+            for (int i = 0; i < argc; i++){
+                work[i] = avaiable[i];
+            }
+
+            for (int i = 0; i < number_of_customers; i++){
+                finish[i] = 0;
+            }
+
+
             
         } else if (strcmp(op, "RL") == 0) {
             int invalido = 0;
