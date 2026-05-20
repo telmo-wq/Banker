0bfc23ba550c9e6225544530f5386c240301f336
telmo-wq <telmomcalheiros@gmail.com>
2026-05-19 23:35:38 -0300

    Saída de dados mais ajustada

--- imprimir_status.c
+++ imprimir_status.c
@@ -2,26 +2,61 @@
 
 
 void imprimir_status(FILE *log, int **max, int **allocation, int **need, int number_of_customers, int argc, int avaiable[]){
-    fprintf(log, "MAXIMUM  | ALLOCATION  | NEED\n");
+    int largura_maxima = 1;
+    for (int i = 0; i < number_of_customers; i++){
+        for (int j = 0; j < argc; j++){
+            int val = max[i][j];
+            int digitos = (val == 0) ? 1 : 0;
+            while (val > 0) { 
+                digitos++; 
+                val /= 10; 
+            }
+            if (digitos > largura_maxima) largura_maxima = digitos;
+            
+            val = allocation[i][j];
+            digitos = (val == 0) ? 1 : 0;
+            while (val > 0) { 
+                digitos++; 
+                val /= 10; 
+            }
+            if (digitos > largura_maxima) largura_maxima = digitos;
+            
+            val = need[i][j];
+            digitos = (val == 0) ? 1 : 0;
+            while (val > 0) { 
+                digitos++; 
+                val /= 10; 
+            }
+            if (digitos > largura_maxima) largura_maxima = digitos;
+        }
+    }
+
+    int largura_bloco = (largura_maxima + 1) * argc - 1;
+    fprintf(log, "%-*s | %-*s | NEED\n", largura_bloco, "MAXIMUM", largura_bloco, "ALLOCATION");
+
+    fprintf(log, "\n");
 
     for (int i = 0; i < number_of_customers; i++){
         //exibindo max do cliente i
         for (int j = 0; j < argc; j++){
-            fprintf(log, "%d ", max[i][j]);
+            fprintf(log, "%*d ", largura_maxima, max[i][j]);
+            if (j < argc - 1) fprintf(log, " ");
         }
         fprintf(log, " | ");
 
         //exibindo o allocation do cliente i
 
         for (int j = 0; j < argc; j++){
-            fprintf(log, "%d ", allocation[i][j]);
+            fprintf(log, "%*d ", largura_maxima, allocation[i][j]);
+            if (j < argc - 1) fprintf(log, " ");
         }
         fprintf(log, " | ");
 
         //exibindo o need do cliente i
 
         for (int j = 0; j < argc; j++){
-            fprintf(log, "%d ", need[i][j]);
+            fprintf(log, "%*d ", largura_maxima, need[i][j]);
+            if (j < argc - 1) fprintf(log, " ");
         }
         fprintf(log, "\n");
     }
